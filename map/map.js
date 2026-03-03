3da58ecf94c1db74a2ca5af4b71a7f5e86d28368
Habeen Lee <your_emailcropvice@icloud.com>
2026-03-03 17:36:11 +0900

    2nd commit

--- map/map.js
+++ map/map.js
@@ -1,17 +1,17 @@
 // 지도 영역 가져오기
-const container = document.getElementById('map');
+const mapArea = document.getElementById('map');
 
 // 지도 옵션
-const options = {
-  center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울시청
+const mapOptions = {
+  center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울시청좌표
   level: 3
 };
 
 // 지도 생성
-const map = new kakao.maps.Map(container, options);
+const drawMap = new kakao.maps.Map(mapArea, mapOptions);
 
 //클릭 위치 마커 생성
-kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
+kakao.maps.event.addListener(drawMap, 'click', function(mouseEvent) {
 
   let latLng = mouseEvent.latLng;
 
@@ -21,15 +21,15 @@ kakao.maps.event.addListener(map, 'click', function(mouseEvent) {
     position: latLng
   });
 
-  marker.setMap(map);
+  marker.setMap(drawMap);
 });
 
 // 확대
 document.getElementById("zoomIn").addEventListener("click", function() {
-    map.setLevel(map.getLevel() - 1);
+    drawMap.setLevel(drawMap.getLevel() - 1);
 });
 
 // 축소
 document.getElementById("zoomOut").addEventListener("click", function() {
-    map.setLevel(map.getLevel() + 1);
+    drawMap.setLevel(drawMap.getLevel() + 1);
 });
